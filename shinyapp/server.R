6c19c6ecb4525c7bf53afa785ec837916d799aa4
CatherineTang <yt2547@columbia.edu>
2017-02-23 16:10:05 -0500

    123

--- shinyapp/server.R
+++ shinyapp/server.R
@@ -66,8 +66,8 @@ apply_params <- function(state, outcome, range) {
 
 
 complications<-read.csv("Complications_Manhattan.csv")
-filename<-"../data/hospital_manhattan_basic_info.csv"
-Manhattan_hospital<-as.data.frame(read.csv(file = filename,header=T))
+filename<-"../data/hospital_manhattan_basic_info1.csv"
+Manhattan_hospital<-read.csv(file = filename,header=T)
 
 new_complications<-as.data.frame(matrix(NA,ncol=length(levels(complications$Measure.Name)),nrow=length(levels(complications$Hospital.Name))))
 colnames(new_complications)<-levels(complications$Measure.Name)
@@ -84,6 +84,8 @@ rownames(new_complications)<-levels(complications$Hospital.Name)
 averageNYCperformance<-rep(6,ncol(new_complications))
 new_complications=rbind(new_complications,averageNYCperformance)
 
+
+
 source('helpers.R')
 library(fields)
 
@@ -91,7 +93,6 @@ library(fields)
 
 shinyServer(function(input, output) {
   
-<<<<<<< HEAD
   currentYear <- reactive({
     input$year
   })
@@ -120,45 +121,6 @@ shinyServer(function(input, output) {
     u1$xAxis(type="addMeasureAxis", outputFormat="#,")
     u1$yAxis(type="addCategoryAxis")
     u1
-=======
-  nyc<-reactive({
-    content1<-paste("<b><a href ='http://www.mountsinai.org'> Mount Sinai Hospital</a><b>","<br/>",
-                    "Average Waiting Time: 29","<br/>", 
-                    "Cost Level: Lower than Average")
-    content2<-paste("<b><a href ='http://www.mountsinai.org/locations/st-lukes'> Mount Sinai St. Luke's Hospital</a><b>","<br/>",
-                    "Average Waiting Time: 24", "<br/>",
-                    "Cost Level: About Average")
-    content3<-paste("<b><a href ='www.nyee.edu'> New York Eyes and Ear Infirmary</a><b>","<br/>",
-                    "Average Waiting Time: NA","<br/>", 
-                    "Cost Level: Above Average")
-    content4<-paste("<b><a href ='http://www.nyp.org/'> New York Presbyterian Hospital</a><b>","<br/>",
-                    "Average Waiting Time: 15", "<br/>",
-                    "Cost Level: About Average")
-    content5<-paste("<b><a href ='https://www.northwell.edu/find-care/locations/lenox-hill-hospital'> Lenox Hill Hospital | Northwell Health</a><b>","<br/>",
-                    "Average Waiting Time: 20", "<br/>",
-                    "Cost Level: Lower than Average")
-    content6<-paste("<b><a href ='http://www.bethisraelny.org/petrie/'> Mount Sinai Beth Israel - Petrie Division</a><b>","<br/>",
-                    "Average Waiting Time: 23", "<br/>",
-                    "Cost Level: Above Average")
-    content7<-paste("<b><a href ='http://www.nychealthandhospitals.org/metropolitan/'> NYC Health + Hospitals| Metropolitan</a><b>","<br/>",
-                    "Average Waiting Time: 50", "<br/>",
-                    "Cost Level: Above Average")
-    content8<-paste("<b><a href ='http://www.nychealthandhospitals.org/bellevue/'> NYC Health + Hospitals| Bellevue</a><b>","<br/>",
-                    "Average Waiting Time: 28", "<br/>",
-                    "Cost Level: NA")
-    content9<-paste("<b><a href ='https://profiles.health.ny.gov/hospital/view/103021'> NYU Hospital Center</a><b>","<br/>",
-                    "Average Waiting Time: 11","<br/>",
-                    "Cost Level: About Average")
-    content10<-paste("<b><a href ='http://www.nychealthandhospitals.org/harlem/'> Harlem Hospital Center</a><b>","<br/>",
-                     "Average Waiting Time: 90", "<br/>",
-                     "Cost Level: Above Average")
-    content11<-paste("<b><a href ='https://www.hss.edu/why-choose-hss.asp?gclid=COP0hOaFndICFQ5YDQodKWcJ4Q'> Hospital for Special Surgery</a><b>","<br/>",
-                     "Average Waiting Time: NA", "<br/>",
-                     "Cost Level: NA")
-    content12<-paste("<b><a href ='https://www.rucares.org/'> The Rockefeller University Hospital</a><b>","<br/>",
-                     "Average Waiting Time: NA", "<br/>",
-                     "Cost Level: Lower than Average")
->>>>>>> origin/master
     
   })
   
@@ -167,32 +129,89 @@ shinyServer(function(input, output) {
   
   
   nyc<-reactive({
+    
+    
+    cost<-c()
+    for (i in 1:nrow(Manhattan_hospital)){
+      if (is.na(Manhattan_hospital$Price.Score[i])==FALSE){
+        if (Manhattan_hospital$Price.Score[i]<0.965){
+          cost[i]<-"Lower than Average"
+        }else if(Manhattan_hospital$Price.Score[i]>=0.965 & Manhattan_hospital$Price.Score[i]<=0.9975){
+          cost[i]<-"About Average"
+        }else{
+          cost[i]<-"Above Average"
+        }
+      }else{
+        cost[i]<="NA"
+      }
+    }
+    Manhattan_hospital<-cbind(Manhattan_hospital,cost)
+    averageWaitingtime<-read.csv("Average_Waitingtime.csv")
+    averageWaitingtime<-averageWaitingtime$Average.WaitingTime
+    
+    content1<-paste("<b><a href =' '> Mount Sinai Hospital</ a><b>","<br/>",
+                    "Average Waiting Time: ",averageWaitingtime[1],"<br/>", 
+                    "Cost Level: ",Manhattan_hospital$cost[1])
+    content2<-paste("<b><a href ='http://www.mountsinai.org/locations/st-lukes'> Mount Sinai St. Luke's Hospital</ a><b>","<br/>",
+                    "Average Waiting Time: ",averageWaitingtime[2], "<br/>",
+                    "Cost Level: ",Manhattan_hospital$cost[2])
+    content3<-paste("<b><a href ='www.nyee.edu'> New York Eyes and Ear Infirmary</ a><b>","<br/>",
+                    "Average Waiting Time: ",averageWaitingtime[3],"<br/>", 
+                    "Cost Level: ",Manhattan_hospital$cost[3])
+    content4<-paste("<b><a href ='http://www.nyp.org/'> New York Presbyterian Hospital</ a><b>","<br/>",
+                    "Average Waiting Time: ",averageWaitingtime[4], "<br/>",
+                    "Cost Level: ",Manhattan_hospital$cost[4])
+    content5<-paste("<b><a href ='https://www.northwell.edu/find-care/locations/lenox-hill-hospital'> Lenox Hill Hospital | Northwell Health</ a><b>","<br/>",
+                    "Average Waiting Time: ",averageWaitingtime[5], "<br/>",
+                    "Cost Level: ",Manhattan_hospital$cost[5])
+    content6<-paste("<b><a href ='http://www.bethisraelny.org/petrie/'> Mount Sinai Beth Israel - Petrie Division</ a><b>","<br/>",
+                    "Average Waiting Time: ",averageWaitingtime[6], "<br/>",
+                    "Cost Level: ",Manhattan_hospital$cost[6])
+    content7<-paste("<b><a href ='http://www.nychealthandhospitals.org/metropolitan/'> NYC Health + Hospitals| Metropolitan</ a><b>","<br/>",
+                    "Average Waiting Time: ",averageWaitingtime[7], "<br/>",
+                    "Cost Level: ",Manhattan_hospital$cost[7])
+    content8<-paste("<b><a href ='http://www.nychealthandhospitals.org/bellevue/'> NYC Health + Hospitals| Bellevue</ a><b>","<br/>",
+                    "Average Waiting Time: ",averageWaitingtime[8], "<br/>",
+                    "Cost Level: ",Manhattan_hospital$cost[8])
+    content9<-paste("<b><a href ='https://profiles.health.ny.gov/hospital/view/103021'> NYU Hospital Center</ a><b>","<br/>",
+                    "Average Waiting Time: ",averageWaitingtime[9],"<br/>",
+                    "Cost Level: ",Manhattan_hospital$cost[9])
+    content10<-paste("<b><a href ='http://www.nychealthandhospitals.org/harlem/'> Harlem Hospital Center</ a><b>","<br/>",
+                     "Average Waiting Time: ",averageWaitingtime[10], "<br/>",
+                     "Cost Level: ",Manhattan_hospital$cost[10])
+    content11<-paste("<b><a href ='https://www.hss.edu/why-choose-hss.asp?gclid=COP0hOaFndICFQ5YDQodKWcJ4Q'> Hospital for Special Surgery</ a><b>","<br/>",
+                     "Average Waiting Time: ",averageWaitingtime[11], "<br/>",
+                     "Cost Level: ",Manhattan_hospital$cost[11])
+    content12<-paste("<b><a href ='https://www.rucares.org/'> The Rockefeller University Hospital</ a><b>","<br/>",
+                     "Average Waiting Time: ",averageWaitingtime[12], "<br/>",
+                     "Cost Level: ",Manhattan_hospital$cost[12])
+    hospital_content<-c(content1,content2,content3,content4,content5,content6,content7,content8,content9,content10,content11,content12)
    
     rank<-seq(1,nrow(Manhattan_hospital))
     # Plot a default web map (brackets display the result)
     (m <- leaflet() %>% addTiles())
-    img <- readPNG("../data/nyc2013_map_results.png")
+    img <- readPNG("nyc2013_map_results.png")
     grid.raster(img)
     
     # map for the whole new york city
     m %>% setView(lng = -73.835242 , lat =  40.740610, zoom = 10)
     
     # map for Manhattan area
-    m%>% setView(lng = -73.9712 , lat =  40.7831, zoom = 12)
-    
-    nyc_map<-m%>% setView(lng = -73.9712 , lat =  40.7831, zoom = 12)
-    (m2 <- m %>% # popup
+    nyc_map<- m %>% setView(lng = -73.9712 , lat =  40.7831, zoom = 12)
+    (m2 <- m 
+      %>% # popup
         addTiles() %>%
         # add som markers:
-        addMarkers(Manhattan_hospital$lng,Manhattan_hospital$lat,popup=hospital_content))
+        addMarkers(Manhattan_hospital$Longitude,Manhattan_hospital$Latitude,popup=hospital_content))
     return(nyc_map)})
   
   output$nyc_map<-renderLeaflet({
     nyc()%>% setView(lng = -73.9712 , lat =  40.7831, zoom = 12)
-    (m2 <- m %>% # popup
+    (m2 <- m 
+      %>% # popup
         addTiles() %>%
         # add som markers:
-        addMarkers(Manhattan_hospital$lng,Manhattan_hospital$lat,popup=hospital_content))
+        addMarkers(Manhattan_hospital$Longitude,Manhattan_hospital$Latitude,popup=hospital_content))
     
   })
     
@@ -264,11 +283,9 @@ shinyServer(function(input, output) {
       (m2 <- m %>% # popup
           addTiles() %>%
           # add som markers:
-          addMarkers(Manhattan_hospital$lng[Manhattan_hospital$Hospital.Name==names(which(new_complications[,input$care]==1))],
-                     Manhattan_hospital$lat[Manhattan_hospital$Hospital.Name==names(which(new_complications[,input$care]==1))]#,
-<<<<<<< Updated upstream
+          addMarkers(Manhattan_hospital$Longitude[Manhattan_hospital$Hospital.Name==names(which(new_complications[,input$care]==1))],
+                     Manhattan_hospital$Latitude[Manhattan_hospital$Hospital.Name==names(which(new_complications[,input$care]==1))]#,
                      #popup=hospital_content[,names(which(new_complications[input$care]==1))]
-<<<<<<< HEAD
                      # addPopups <- function(
                      #   m, lng = Manhattan_hospital$lng[Manhattan_hospital$Hospital.Name==names(which(new_complications[,input$care]==1))], 
                      #   lat = Manhattan_hospital$lat[Manhattan_hospital$Hospital.Name==names(which(new_complications[,input$care]==1))], popup, layerId = NULL, group = NULL,
@@ -284,14 +301,6 @@ shinyServer(function(input, output) {
         
         
       })
-=======
-=======
-                     #popup = hospital_content[,names(which(new_complications[input$care]==1))]
-                  
->>>>>>> Stashed changes
-                     
-          ))})
->>>>>>> origin/master
     
    
 
