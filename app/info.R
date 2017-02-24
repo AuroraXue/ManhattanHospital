57b2f5e8b4e02a604807d1fb2726a159214b9218
BoyaZhao <zhaoby1994@gmail.com>
2017-02-24 11:06:12 -0500

    z

--- app/info.R
+++ app/info.R
@@ -2,25 +2,24 @@ url.data <- 'https://data.medicare.gov/data/hospital-compare'
 url.sourcecode <- 'dummy url'
 
 info <- list(
-  br(),
-  h4('NYC Hospital Care Outcomes- ShinyApp'),
-  p('About:'),
-  br(),
-  p('Looking At Mortality and Readmission Rates in NYC Hospitals.'),
-  hr(),
-  br(),
+br(),
+h4('QUALICARE'),
+p('This app pay attention to patient safety measure for the hospital quality, based on HAI(Hospital Associated Infection), mortality estimate and main complications. Provide information, like rank and value plot about the predict mortality in last 30 days. Besides, give the radar plot about main complication may happen in the hospital. And make recommendation about the hospital based on customer\'s concern. For the third part, this app provides the visual bar plot about the history data of HAI to help customers to better decide which hospital to go.
+'),
+hr(),
+br(),
 
-  tag('ul', list(
-    
-    tag('li', list('Date', a(href=url.data, url.data))),
-    tag('li', list('To Find Under', strong('CSV Flat Files - Revised'))) #,
+tag('ul', list(
 
-  )),
-  br(),
-  p('This Shiny App Was Made For Applied STATGU 5243 At Columbia University'),
-  #p('link zu Sourcecode - GitHub:'),
-  #p(a(url.sourcecode, href=url.sourcecode)),
-  hr(),
-  p('Reference', a('Shiny RStudio', href='http://shiny.rstudio.com/'))
+tag('li', list('Date', a(href=url.data, url.data))),
+tag('li', list('To Find Under', strong('CSV Flat Files - Revised'))) #,
+
+)),
+br(),
+p('This Shiny App Was Made For Applied STATGU 5243 At Columbia University'),
+#p('link zu Sourcecode - GitHub:'),
+#p(a(url.sourcecode, href=url.sourcecode)),
+hr(),
+p('Reference', a('Shiny RStudio', href='http://shiny.rstudio.com/'))
 )
 
